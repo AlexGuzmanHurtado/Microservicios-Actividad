0c77df03ba3ca5f5d0ae9ed90400c2a77406408f
AlexGuzmanHurtado <alexguzmanxp12@gmail.com>
2024-05-18 02:24:56 -0500

    Dockerfile

--- python/src/auth/init.sql
+++ python/src/auth/init.sql
@@ -1,4 +1,4 @@
-CREATE USER 'auth_user'@'localhost' IDENTIFIED BY 'Auth123';
+CREATE USER 'auth_user'@'localhost' IDENTIFIED BY 'Aauth123';
 
 CREATE DATABASE auth;
 
@@ -7,9 +7,9 @@ GRANT ALL PRIVILEGES ON auth.* TO 'auth_user'@'localhost';
 USE auth;
 
 CREATE TABLE user (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  email VARCHAR(255) NOT NULL UNIQUE,
-  password VARCHAR(255) NOT NULL
+    id INT NOT NULL AUT_INCRIMENT PRIMARY KEY, 
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
 );
 
-INSERT INTO user (email, password) VALUES ('georgio@email.com', 'Admin123');
+INSERT INTO user (email, password) VALUES ('alexguzmanxp12@gmail.com', 'Admin123')
